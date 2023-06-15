210269097041b88ba0a23819d878b073050770ee
Damien Clément <damien.clement.czech@gmail.com>
2023-06-15 14:03:12 +0200

    chore: Reimplement better implicit if operators

--- impl/src/main/scala/com/github/derghust/flinkblaze/impl/IfOperatorImpl.scala
+++ impl/src/main/scala/com/github/derghust/flinkblaze/impl/IfOperatorImpl.scala
@@ -1,7 +1,15 @@
 package com.github.derghust.flinkblaze.impl
 
-import com.github.derghust.flinkblaze.impl.AsyncOperatorImpl.{AsyncOperator, AsyncOrderType, AsyncOrderTypeWithRetry}
-import org.apache.flink.api.common.functions.{FilterFunction, FlatMapFunction, MapFunction}
+import com.github.derghust.flinkblaze.impl.AsyncOperatorImpl.{
+  AsyncOperator,
+  AsyncOrderType,
+  AsyncOrderTypeWithRetry
+}
+import org.apache.flink.api.common.functions.{
+  FilterFunction,
+  FlatMapFunction,
+  MapFunction
+}
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.scala.DataStream
 import org.apache.flink.streaming.api.scala.async.{AsyncFunction, AsyncRetryStrategy}
@@ -9,7 +17,7 @@ import org.apache.flink.streaming.api.scala.async.{AsyncFunction, AsyncRetryStra
 import scala.concurrent.duration.TimeUnit
 
 object IfOperatorImpl {
-  implicit class IfOperator[IN](ds: DataStream[IN]) {
+  implicit class IfOperator[IN: TypeInformation](ds: DataStream[IN]) {
 
     /** Creates a new DataStream by applying the given function to every element of this
       * DataStream only if state is true.
@@ -18,20 +26,43 @@ object IfOperatorImpl {
       *   to use
       * @param state
       *   State from which will be decided if
-      * @tparam OUT
-      *   Type of the output record
       * @return
       *   The resulting stream containing the asynchronous results, otherwise if state
-      *   is false return unchanged [[DataStream]] with type information of [[OUT]]
+      *   is false return unchanged [[DataStream]].
+      */
+    def mapIf(
+        function: MapFunction[IN, IN],
+        state: Boolean
+    ): DataStream[IN] = {
+      if (state) {
+        ds.map(function)
+      } else {
+        ds
+      }
+    }
+
+    /** Creates a new DataStream by applying the given function to every element of this
+      * DataStream only if state is true.
+      *
+      * @param function
+      *   to use.
+      * @param state
+      *   State from which will be decided if.
+      * @tparam OUT
+      *   Type of the output record.
+      * @return
+      *   The resulting stream containing the asynchronous results as [[Right]]
+      *   [[DataStream]] side with [[OUT]] type information, otherwise if state is false
+      *   return unchanged as [[Left]] [[DataStream]] with type information of [[OUT]].
       */
     def mapIf[OUT: TypeInformation](
         function: MapFunction[IN, OUT],
         state: Boolean
-    ): DataStream[OUT] = {
+    ): Either[DataStream[IN], DataStream[OUT]] = {
       if (state) {
-        ds.map(function)
+        Right(ds.map(function))
       } else {
-        ds.asInstanceOf[DataStream[OUT]]
+        Left(ds)
       }
     }
 
@@ -39,23 +70,46 @@ object IfOperatorImpl {
       * flattening the results only if state is true.
       *
       * @param function
-      *   to use
+      *   to use.
       * @param state
-      *   State from which will be decided if
-      * @tparam OUT
-      *   Type of the output record
+      *   State from which will be decided if.
       * @return
       *   The resulting stream containing the asynchronous results, otherwise if state
-      *   is false return unchanged [[DataStream]] with type information of [[OUT]]
+      *   is false return unchanged [[DataStream]].
+      */
+    def flatMapIf(
+        function: FlatMapFunction[IN, IN],
+        state: Boolean
+    ): DataStream[IN] = {
+      if (state) {
+        ds.flatMap(function)
+      } else {
+        ds
+      }
+    }
+
+    /** Creates a new DataStream by applying the given function to every element and
+      * flattening the results only if state is true.
+      *
+      * @param function
+      *   to use.
+      * @param state
+      *   State from which will be decided if.
+      * @tparam OUT
+      *   Type of the output record.
+      * @return
+      *   The resulting stream containing the asynchronous results as [[Right]]
+      *   [[DataStream]] side with [[OUT]] type information, otherwise if state is false
+      *   return unchanged as [[Left]] [[DataStream]] with type information of [[OUT]].
       */
     def flatMapIf[OUT: TypeInformation](
         function: FlatMapFunction[IN, OUT],
         state: Boolean
-    ): DataStream[OUT] = {
+    ): Either[DataStream[IN], DataStream[OUT]] = {
       if (state) {
-        ds.flatMap(function)
+        Right(ds.flatMap(function))
       } else {
-        ds.asInstanceOf[DataStream[OUT]]
+        Left(ds)
       }
     }
 
@@ -63,12 +117,12 @@ object IfOperatorImpl {
       * filter predicate only if state is true.
       *
       * @param function
-      *   to use
+      *   to use.
       * @param state
-      *   State from which will be decided if
+      *   State from which will be decided if.
       * @return
       *   The resulting stream containing the asynchronous results, otherwise if state
-      *   is false return unchanged [[DataStream]]
+      *   is false return unchanged [[DataStream]].
       */
     def filterIf(
         function: FilterFunction[IN],
@@ -86,14 +140,14 @@ object IfOperatorImpl {
       * aggregations only if state is true.
       *
       * @param function
-      *   to use
+      *   to use.
       * @param state
-      *   State from which will be decided if
+      *   State from which will be decided if.
       * @tparam K
-      *   Type of the keyed record
+      *   Type of the keyed record.
       * @return
       *   The resulting stream containing the asynchronous results, otherwise if state
-      *   is false return unchanged [[DataStream]]
+      *   is false return unchanged [[DataStream]].
       */
     def keyByIf[K: TypeInformation](
         function: IN => K,
@@ -111,40 +165,81 @@ object IfOperatorImpl {
       * help you simplify and clean your code. Apply an asynchronous function on the
       * input data stream. The output order is only maintained with respect to
       * watermarks. Stream records which lie between the same two watermarks, can be
-      * re-ordered
+      * re-ordered.
       *
       * @param asyncFunction
-      *   to use
+      *   to use.
       * @param orderType
       *   control order of the records.
-      *   [[https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/dev/datastream/operators/asyncio/#order-of-results]]
+      *   [[https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/dev/datastream/operators/asyncio/#order-of-results]].
       * @param timeout
-      *   for the asynchronous operation to complete
+      *   for the asynchronous operation to complete.
       * @param timeUnit
-      *   of the timeout
+      *   of the timeout.
       * @param capacity
       *   of the operator which is equivalent to the number of concurrent asynchronous
-      *   operations
+      *   operations.
       * @param state
-      *   State from which will be decided if
-      * @tparam OUT
-      *   Type of the output record
+      *   State from which will be decided if.
       * @return
       *   the resulting stream containing the asynchronous results, otherwise if state
-      *   is false return unchanged [[DataStream]] with type information of [[OUT]]
+      *   is false return unchanged [[DataStream]].
       */
-    def asyncIf[OUT: TypeInformation](
+    def asyncIf(
+        asyncFunction: AsyncFunction[IN, IN],
+        orderType: AsyncOrderType,
+        timeout: Long,
+        timeUnit: TimeUnit,
+        capacity: Int = 100,
+        state: Boolean
+    ): DataStream[IN] = {
+      if (state) {
+        ds.async(asyncFunction, orderType, timeout, timeUnit, capacity)
+      } else {
+        ds
+      }
+    }
+
+    /** Creates a new DataStream by applying the given function to every element of this
+      * DataStream only if state is true. Wrapper method for AsyncDataStream that will
+      * help you simplify and clean your code. Apply an asynchronous function on the
+      * input data stream. The output order is only maintained with respect to
+      * watermarks. Stream records which lie between the same two watermarks, can be
+      * re-ordered. Capital E at the end represent Either type.
+      *
+      * @param asyncFunction
+      *   to use.
+      * @param orderType
+      *   control order of the records.
+      *   [[https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/dev/datastream/operators/asyncio/#order-of-results]].
+      * @param timeout
+      *   for the asynchronous operation to complete.
+      * @param timeUnit
+      *   of the timeout.
+      * @param capacity
+      *   of the operator which is equivalent to the number of concurrent asynchronous
+      *   operations.
+      * @param state
+      *   State from which will be decided if.
+      * @tparam OUT
+      *   Type of the output record.
+      * @return
+      *   The resulting stream containing the asynchronous results as [[Right]]
+      *   [[DataStream]] side with [[OUT]] type information, otherwise if state is false
+      *   return unchanged as [[Left]] [[DataStream]] with type information of [[OUT]].
+      */
+    def asyncIfE[OUT: TypeInformation](
         asyncFunction: AsyncFunction[IN, OUT],
         orderType: AsyncOrderType,
         timeout: Long,
         timeUnit: TimeUnit,
         capacity: Int = 100,
         state: Boolean
-    ): DataStream[OUT] = {
+    ): Either[DataStream[IN], DataStream[OUT]] = {
       if (state) {
-        ds.async(asyncFunction, orderType, timeout, timeUnit, capacity)
+        Right(ds.async(asyncFunction, orderType, timeout, timeUnit, capacity))
       } else {
-        ds.asInstanceOf[DataStream[OUT]]
+        Left(ds)
       }
     }
 
@@ -161,31 +256,29 @@ object IfOperatorImpl {
       *   control order of the records.
       *   [[https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/dev/datastream/operators/asyncio/#order-of-results]]
       * @param timeout
-      *   for the asynchronous operation to complete
+      *   for the asynchronous operation to complete.
       * @param timeUnit
-      *   of the timeout
+      *   of the timeout.
       * @param capacity
       *   of the operator which is equivalent to the number of concurrent asynchronous
-      *   operations
-      * @tparam OUT
-      *   Type of the output record
+      *   operations.
       * @param asyncRetryStrategy
-      *   The strategy of reattempt async i/o operation that can be triggered
+      *   The strategy of reattempt async i/o operation that can be triggered.
       * @param state
-      *   State from which will be decided if
+      *   State from which will be decided if.
       * @return
       *   the resulting stream containing the asynchronous results, otherwise if state
-      *   is false return unchanged [[DataStream]] with type information of [[OUT]]
+      *   is false return unchanged [[DataStream]].
       */
-    def asyncWithRetryIf[OUT: TypeInformation](
-        asyncFunction: AsyncFunction[IN, OUT],
+    def asyncWithRetryIf(
+        asyncFunction: AsyncFunction[IN, IN],
         orderType: AsyncOrderTypeWithRetry,
         timeout: Long,
         timeUnit: TimeUnit,
         capacity: Int = 100,
-        asyncRetryStrategy: AsyncRetryStrategy[OUT],
+        asyncRetryStrategy: AsyncRetryStrategy[IN],
         state: Boolean
-    ): DataStream[OUT] = {
+    ): DataStream[IN] = {
       if (state) {
         ds.asyncWithRetry(
           asyncFunction,
@@ -196,7 +289,62 @@ object IfOperatorImpl {
           asyncRetryStrategy
         )
       } else {
-        ds.asInstanceOf[DataStream[OUT]]
+        ds
+      }
+    }
+
+    /** Creates a new DataStream by applying the given function to every element of this
+      * DataStream only if state is true. Wrapper method for AsyncDataStream that will
+      * help you simplify and clean your code. Apply an asynchronous function on the
+      * input data stream. The output order is only maintained with respect to
+      * watermarks. Stream records which lie between the same two watermarks, can be
+      * re-ordered. Capital E at the end represent Either type.
+      *
+      * @param asyncFunction
+      *   to use.
+      * @param orderType
+      *   control order of the records.
+      *   [[https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/dev/datastream/operators/asyncio/#order-of-results]].
+      * @param timeout
+      *   for the asynchronous operation to complete.
+      * @param timeUnit
+      *   of the timeout.
+      * @param capacity
+      *   of the operator which is equivalent to the number of concurrent asynchronous
+      *   operations.
+      * @tparam OUT
+      *   Type of the output record.
+      * @param asyncRetryStrategy
+      *   The strategy of reattempt async i/o operation that can be triggered.
+      * @param state
+      *   State from which will be decided if.
+      * @return
+      *   The resulting stream containing the asynchronous results as [[Right]]
+      *   [[DataStream]] side with [[OUT]] type information, otherwise if state is false
+      *   return unchanged as [[Left]] [[DataStream]] with type information of [[OUT]].
+      */
+    def asyncWithRetryIfE[OUT: TypeInformation](
+        asyncFunction: AsyncFunction[IN, OUT],
+        orderType: AsyncOrderTypeWithRetry,
+        timeout: Long,
+        timeUnit: TimeUnit,
+        capacity: Int = 100,
+        asyncRetryStrategy: AsyncRetryStrategy[OUT],
+        state: Boolean
+    ): Either[DataStream[IN], DataStream[OUT]] = {
+      if (state) {
+        Right(
+          ds.asyncWithRetry(
+            asyncFunction,
+            orderType,
+            timeout,
+            timeUnit,
+            capacity,
+            asyncRetryStrategy
+          )
+        )
+      } else {
+        Left(ds)
       }
     }
   }
